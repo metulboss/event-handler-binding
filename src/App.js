7fb7ef85e20c7344cd3b9bcd5466fb5625ccde39
metulboss <metulromel@gmail.com>
2022-07-17 16:24:03 +0600

    binding

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Eventbinding from "./components/eventbinding/Eventbinding"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Event Handler Binding</h1>
+      <Eventbinding></Eventbinding>
     </div>
   );
 }
